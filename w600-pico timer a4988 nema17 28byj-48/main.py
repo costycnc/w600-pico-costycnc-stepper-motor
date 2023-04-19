fa2254a3b87cd332bf01891e9cd5ee5584f6b9ba
Boboaca Costel <bobyca2003@yahoo.com>
2023-04-19 22:39:48 +0200

    Add files via upload

--- w600-pico timer a4988 nema17 28byj-48/main.py
+++ w600-pico timer a4988 nema17 28byj-48/main.py
@@ -2,9 +2,19 @@ from machine import Pin
 from machine import Timer
 
 step = Pin(Pin.PB_18, Pin.OUT, Pin.PULL_FLOATING)
+direction = Pin(Pin.PA_00, Pin.OUT, Pin.PULL_FLOATING)
 
 timer3 = Timer(3)
 timer3.init(period=1, mode=Timer.PERIODIC, callback=lambda t:prt())
+a=0
 
 def prt():
+    global a
+    a+=1
     step.value(not step.value())
+    if a>2000:
+        direction.value(0)
+        if a>4000:
+            direction.value(1)
+            a=0
+        
