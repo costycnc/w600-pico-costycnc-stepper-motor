d796017b75152448d0961d811e4ef8a3c0125ead
Boboaca Costel <bobyca2003@yahoo.com>
2023-04-28 19:02:43 +0200

    terminat x or y ... x and y
    Adesso quando x e y hanno valori ... movimento una asse fa in funzie di altra asse
    Essempio: Se X=10 e Y=3 ... asse x fa 3 step e asse y solo uno ... targhet sara 9 per asse x  invece
    di 10 ... per dividersi esatto e finire in numeri interi senza virgula

--- lasergrbl costycnc/main.py
+++ lasergrbl costycnc/main.py
@@ -1,5 +1,6 @@
+# main.py -- put your code here!
 # costycnc gcode interpreter 1.0
-import usocket as socket
+import socket
 import time
 
 s = socket.socket()
@@ -16,8 +17,15 @@ currenty=0
 directiony=0
 buffer=[]
 absolute=0
+kx=0
+ky=0
 ax=""
 b=[]
+este=0
+movementx=0
+movementy=0
+tmpx=0
+tmpy=0
 
 def extract(a4):
     vrt=""
@@ -29,65 +37,82 @@ def extract(a4):
     return vrt    
 
 def gcode_exec(strg):
-    global targhetx,currentx,targhety,currenty,directionx,directiony,absolute
-
+    global targhetx,currentx,targhety,currenty,directionx,directiony,absolute,k,mx,my,tmpx,tmpy
+    print(strg)
+    este=0
     if "G90" in strg:
-        print("absolute")
-        absolute=0
+        absolute=1
         
     if "G91" in strg:
-        print("incremental")
-        absolute=1       
-
+        absolute=0       
+    mx=0
+    my=0
+    tmpx=0
+    tmpy=0
     if ("X") in strg:
+        targhetx=0
         nn=strg.split("X")
-        k=int(float(extract(nn[1]))) 
-        print("arivatto=",k)
-        if absolute:                 # incremental
+        k=int(float(extract(nn[1])))
+        if absolute:
+            targhetx=k             
+        else:
             targhetx=currentx+k
-            print("targhet incr=",targhet)
-            if targhetx>currentx:
-                directionx=1
-            else:
-                directionx=0                           
-        else :                        #absolute
-            targhet=k
-            print("targhet abs=",targhet)
-            if targhetx>currentx:
-                directionx=1
-            else:
-                directionx=0          
-
+        tmpx=1
+        mx=targhetx-currentx
+        if targhetx>currentx:
+            directionx=1
+        else:
+            directionx=-1 
+  
     if ("Y") in strg:
+        targhety=0
         nn=strg.split("Y")
-        k=int(float(extract(nn[1]))) 
-        print("arivatto=",k)
-        if absolute:                 # incremental
-            targhet=current+k
-            print("targhet incr=",targhet)
-            if targhet>current:
-                direction=1
-            else:
-                direction=0                           
-        else :                        #absolute
-            targhet=k
-            print("targhet abs=",targhet)
-            if targhet>current:
-                direction=1
-            else:
-                direction=0                         
+        k=int(float(extract(nn[1])))
+        if absolute:
+            targhety=k
+        else:
+            targhety=currenty+k 
+        tmpy=1
+        my=targhety-currenty    
+        if targhety>currenty:
+            directiony=1
+        else:
+            directiony=-1 
+    print("prima targhetx=",targhetx)
+    print("prima targhety=",targhety)    
+    if (mx and my):
+        if mx>my:            # ex: first X=0 Y=0  -> dopo X=10 Y=3
+            tmpx=int(mx/my)  # 10/3=3.3 -> int(3.3)=3 -> tmpx=3
+            targhetx=tmpx*my # tmpx=3 my=3 -> 3*3=9 -> targhetx=9
+            #directionx is 1 or -1
+            directiony=directiony*tmpx # directiony=1 tmpx=3 -> 1*3=3 -> directiony=3
+            #so ... at 3 step x will make one step y
+        if my>mx: 
+            tmpy=int(my/mx)
+            targhety=tmpy*mx
+            directionx=directionx*tmpy            
+    print("dopo targhetx=",targhetx)
+    print("dopo targhety=",targhety)      
+                      
 
 def do_step():
-    global currentx,directionx,currenty,directiony
-    if directionx:
-        currentx +=1
-    else:
-        currentx -=1
-    print("do_step-current",current)
+    global currentx,directionx,currenty,directiony,targhetx,targhety
+
+
+    if mx:
+        currentx +=directionx
+
+    if my:        
+        currenty +=directiony
+  
+    print("currentx=",currentx," targhetx=",targhetx)
+    print("currenty=",currenty," targhety=",targhety)
+
+    
     time.sleep(.1)
 
 while True:
-    if current==targhet: # arrivatto in punto di destinazione
+    if (currentx==targhetx) and (currenty==targhety): # arrivatto in punto di destinazione
         if len(buffer)>0: # daca buffer e gol atunci sare peste asta
             gcode_exec(buffer.pop(0)) 
             conn.send("ok\n")   
